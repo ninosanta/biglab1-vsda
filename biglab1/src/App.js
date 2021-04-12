83309afaae66ff7e1cca2674b55550c598a82147
francescoalagna <s265350@studenti.polito.it>
2021-04-12 17:35:57 +0200

    icons added and layout change

--- biglab1/src/App.js
+++ biglab1/src/App.js
@@ -1,36 +1,46 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, ListGroup, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, Image, ButtonGroup, Button, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import Task from './Task';
 
 function App() {
   return (
     <Container fluid={true} className="p-0 m-0">
-        <Navbar bg="primary" className="col-12">
-          <Navbar.Brand href="#home">
-            ToDo Manager
-          </Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
-        </Navbar>
       <Row>
-        <Col xs={3}>
-          <aside className="collapse d-sm-block col-sm-4 col-12 bg-primary" id="filter-sidebar" style={{minHeight: '100vh', minWidth: '100%'}}>
-            <ListGroup variant="flush">
-              <ListGroup.Item id="filter-all" className="list-group-item list-group-item-action active">All</ListGroup.Item>
-              <ListGroup.Item id="filter-all" className="list-group-item list-group-item-action">Important</ListGroup.Item>
-              <ListGroup.Item id="filter-all" className="list-group-item list-group-item-action">Today</ListGroup.Item>
-              <ListGroup.Item id="filter-all" className="list-group-item list-group-item-action">This week</ListGroup.Item>
-              <ListGroup.Item id="filter-all" className="list-group-item list-group-item-action">Private</ListGroup.Item>
-            </ListGroup>
+        <Col xs={1}>
+          <aside className="collapse d-sm-block bg-primary align-items-center text-center" id="filter-sidebar" style={{minHeight: '100vh', minWidth: '100%'}}>
+            <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-home">Home</Tooltip>}>
+              <Button id="home" className="mt-4" variant="link"><i class="bi bi-list-nested text-dark" aria-label="Home" style={{fontSize: '1.5rem'}}></i></Button>
+            </OverlayTrigger>
+            <ButtonGroup vertical className="m-1">
+              <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-all">All inbox</Tooltip>}>
+                <Button id="filter-all" className="mt-4" variant="link"><i class="bi bi-inbox text-light" aria-label="All" style={{fontSize: '1.5rem'}}></i></Button>
+              </OverlayTrigger>
+              <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-all">Important</Tooltip>}>
+                <Button id="filter-all" className="mt-4" variant="link"><i class="bi bi-bookmark-star text-light" aria-label="Important" style={{fontSize: '1rem'}}></i></Button>
+              </OverlayTrigger>
+              <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-all">Today</Tooltip>}>
+                <Button id="filter-all" className="mt-4" variant="link"><i class="bi bi-sunset text-light" aria-label="Today" style={{fontSize: '1rem'}}></i></Button>
+              </OverlayTrigger>
+              <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-all">This week</Tooltip>}>
+                <Button id="filter-all" className="mt-4" variant="link"><i class="bi bi-calendar-week text-light" aria-label="This week" style={{fontSize: '1rem'}}></i></Button>
+              </OverlayTrigger>
+              <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-all">Private</Tooltip>}>
+                <Button id="filter-all" className="mt-4" variant="link"><i class="bi bi-eye-slash text-light" aria-label="Private" style={{fontSize: '1rem'}}></i></Button>
+              </OverlayTrigger>
+              <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-all">Search</Tooltip>}>
+                <Button id="filter-search" className="mt-4" variant="link"><i class="bi bi-search text-light" aria-label="Search" style={{fontSize: '1rem'}}></i></Button>
+              </OverlayTrigger>
+            </ButtonGroup>
+            <OverlayTrigger placement="right" overlay={<Tooltip id="image-tooltip-profile">Profile</Tooltip>}>
+              <Image className="align-bottom mt-4" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" roundedCircle style={{width: '50%', height: 'auto'}}></Image>
+            </OverlayTrigger>
+            <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-options">Options</Tooltip>}>
+              <Button id="options" className="align-bottom mt-4" variant="link"><i class="bi bi-three-dots text-light" aria-label="Options" style={{fontSize: '1.5rem'}}></i></Button>
+            </OverlayTrigger>
           </aside>
         </Col>
-        <Col>
-          <h1 className="pt-4">Title</h1>
+        <Col className="mr-4">
+          <h1 className="mt-4">Title</h1>
           <ListGroup variant="flush">
             <Task id={0} label="blank task"/>
             <Task id={1} label="task1" projectBadge='project' dateBadge='Today at 14:00'/>
