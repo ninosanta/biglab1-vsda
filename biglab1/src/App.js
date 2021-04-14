6f9358cc1cff3b4d5db2433c3c62dac8462de59b
mrwasp98 <antoves1998@gmail.com>
2021-04-14 21:54:40 +0200

    Immagini
    Migliorato barra collapse

--- biglab1/src/App.js
+++ biglab1/src/App.js
@@ -1,50 +1,61 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Col, ListGroup, Image, ButtonGroup, Button, OverlayTrigger, Tooltip, ButtonToolbar } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, Image, ButtonGroup, Button, Card, ButtonToolbar } from 'react-bootstrap';
 import Task from './Task';
+import img from './foto.png'
+import img2 from './foto2.jpeg'
 
 function App() {
   return (
-    <Container fluid={true} className="p-0 m-0">
+    <Container fluid={true} className="pe-3 m-0">
       <Row>
-        <Col md={1} xs={1} className="p-0 m-0">
+        <Col md={1} xs={1} className="p-0">
           <aside className="bg-primary align-items-center text-center mx-auto" id="filter-sidebar" style={{ minHeight: '100vh', minWidth: '100%' }}>
             <div>
               <Button id="home" className="mt-4 p-0" variant="link"><i className="bi bi-list-nested text-dark" aria-label="Home" style={{ fontSize: '1.5rem' }}></i></Button>
             </div>
             <div>
               <ButtonGroup vertical className="m-0 w-100 pt-5">
-                <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-all">All inbox</Tooltip>}>
-                  <Button id="filter-all" className="p-3  btn-primary" variant="link"><i className="bi bi-inbox text-light d-flex justify-content-center" aria-label="All" style={{ fontSize: '1.5em'}}></i></Button>
-                </OverlayTrigger>
-                <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-important">Important</Tooltip>}>
-                  <Button id="filter-important" className="p-3 btn-primary" variant="link"><i className="bi bi-bookmark-star text-light d-flex justify-content-center" aria-label="Important" style={{ fontSize: '1.5rem' }}></i></Button>
-                </OverlayTrigger>
-                <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-today">Today</Tooltip>}>
-                  <Button id="filter-today" className="p-3 btn-primary" variant="link"><i className="bi bi-sunset text-light d-flex justify-content-center" aria-label="Today" style={{ fontSize: '1.5rem' }}></i></Button>
-                </OverlayTrigger>
-                <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-week">This week</Tooltip>}>
-                  <Button id="filter-week" className="p-3 btn-primary" variant="link"><i className="bi bi-calendar-week text-light d-flex justify-content-center" aria-label="This week" style={{ fontSize: '1.5rem' }}></i></Button>
-                </OverlayTrigger>
-                <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-private">Private</Tooltip>}>
-                  <Button id="filter-private" className="p-3 btn-primary" variant="link"><i className="bi bi-eye-slash text-light d-flex justify-content-center" aria-label="Private" style={{ fontSize: '1.5rem' }}></i></Button>
-                </OverlayTrigger>
-                <OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-all">Search</Tooltip>}>
-                  <Button id="filter-search" className="p-3 btn-primary" variant="link"><i className="bi bi-search text-light d-flex justify-content-center" aria-label="Search" style={{ fontSize: '1.5rem' }}></i></Button>
-                </OverlayTrigger>
+                <Button id="filter-all" className="p-3  btn-primary" variant="link"><i className="bi bi-inbox text-light d-flex justify-content-center" aria-label="All" style={{ fontSize: '1.5em' }}></i></Button>
+                <Button id="filter-important" className="p-3 btn-primary" variant="link"><i className="bi bi-bookmark-star text-light d-flex justify-content-center" aria-label="Important" style={{ fontSize: '1.5rem' }}></i></Button>
+                <Button id="filter-today" className="p-3 btn-primary" variant="link"><i className="bi bi-sunset text-light d-flex justify-content-center" aria-label="Today" style={{ fontSize: '1.5rem' }}></i></Button>
+                <Button id="filter-week" className="p-3 btn-primary" variant="link"><i className="bi bi-calendar-week text-light d-flex justify-content-center" aria-label="This week" style={{ fontSize: '1.5rem' }}></i></Button>
+                <Button id="filter-private" className="p-3 btn-primary" variant="link"><i className="bi bi-eye-slash text-light d-flex justify-content-center" aria-label="Private" style={{ fontSize: '1.5rem' }}></i></Button>
+                <Button id="filter-search" className="p-3 btn-primary" variant="link"><i className="bi bi-search text-light d-flex justify-content-center" aria-label="Search" style={{ fontSize: '1.5rem' }}></i></Button>
               </ButtonGroup>
             </div>
-            <div><OverlayTrigger placement="right" overlay={<Tooltip id="button-tooltip-options">Options</Tooltip>}>
+            <div>
               <Button id="options" className="p-3 btn-primary w-100" variant="link"><i className="bi bi-three-dots text-light d-flex justify-content-center" aria-label="Options" style={{ fontSize: '1.5rem' }}></i></Button>
-            </OverlayTrigger>
             </div>
-            <div style={{position: "absolute", bottom: "0px"}}>
-              <OverlayTrigger placement="right" overlay={<Tooltip id="image-tooltip-profile">Profile</Tooltip>}>
-                <Image className="w-80 mb-5" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" roundedCircle style={{ width: '50%', height: 'auto' }}></Image>
-              </OverlayTrigger>
+            <div style={{ position: "absolute", bottom: "2rem" }} className="p-0">
+              <Image src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" roundedCircle style={{ width: '65%', height: 'auto' }}></Image>
             </div>
           </aside>
         </Col>
-        <Col md={3} id="collapse-col" className="collapse bg-info align-items-center text-center p-2 d-md-block">
+        <Col md={3} id="collapse-col" className="collapse bg-light align-items-center text-center d-md-block">
+          <div className="d-flex justify-content-center pt-3">
+            <Card style={{ width: '18rem'}}>
+              <Card.Img variant="top" src={img} />
+              <Card.Body>
+                <Card.Title>Web Application I</Card.Title>
+                <Card.Text>
+                  ...
+              </Card.Text>
+                <Button variant="primary">Visualize</Button>
+              </Card.Body>
+            </Card>
+          </div>
+          <div className="d-flex justify-content-center pt-3">
+            <Card style={{ width: '18rem'}}>
+              <Card.Img variant="top" src={img2} />
+              <Card.Body>
+                <Card.Title>Programmazione Di Sistema</Card.Title>
+                <Card.Text>
+                  ...
+              </Card.Text>
+                <Button variant="primary">Visualize</Button>
+              </Card.Body>
+            </Card>
+          </div>
         </Col>
         <Col md={8} xs="auto" className="mr-4" className="p-5 m-0">
           <ButtonToolbar aria-label="Toolbar with button groups" className="justify-content-end">
@@ -65,7 +76,7 @@ function App() {
           </ListGroup>
         </Col>
       </Row>
-    </Container>
+    </Container >
   );
 }
 
