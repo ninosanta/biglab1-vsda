7ef5f6164f32579370547fb8d8488eb5af7cb71a
francescoalagna <s265350@studenti.polito.it>
2021-04-18 18:36:48 +0200

    setOpen

--- biglab1/src/App.js
+++ biglab1/src/App.js
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { Container, Row, Col, ListGroup, ButtonGroup, Button, ButtonToolbar } from 'react-bootstrap';
+import { Container, Row, Col, Collapse, ListGroup, ButtonGroup, Button, ButtonToolbar } from 'react-bootstrap';
 import Task from './Task';
 import NavBarFilters from './NavBarFilters';
 import NavBarProjects from './NavBarProjects';
@@ -20,13 +20,34 @@ const filters = [
   {label: "Today's", icon: 'sunset'},
   {label: "Next week's", icon: 'calendar-week'},
   {label: "Private", icon: 'eye-slash'},
-]
+];
 
 const otherFilters = ['Morning','Afternoon','Evening','Night'];
+
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <Container fluid={true} className="pe-3 m-0">
-      <Row className="collapse d-block d-md-none bg-primary"><NavBarMobile filters={filters}/></Row>
+      <Row className="collapse d-block d-md-none bg-primary"><NavBarMobile open={open} setOpen={setOpen} filters={filters}/></Row>
+      <Collapse in={open}>
+        <Row id="mobile-filter-navbar" className="flex-column bg-primary position-fixed text-center" style={{ minHeight: '100%', width: '5rem'}}>
+            <ButtonGroup vertical className="w-100 pt-5 align-items-center">
+                {filters.map(filter => {
+                    return <Button
+                        id={`filter-${filter.label}`} 
+                        key={`filter-${filter.label}`} 
+                        className="p-3 btn-primary" 
+                        variant="link" 
+                        onClick={() => {document.getElementById('filter-title').innerText = filter.label;}}>
+                            <i className={`bi bi-${filter.icon} text-light d-flex justify-content-center`} aria-label={filter.label} style={{ fontSize: '1.5em' }}></i>
+                    {filter.label}</Button>;
+                })}
+                
+                <Button id="options" key="options" className="p-3 btn-primary w-100" variant="link"><i className="bi bi-three-dots text-light d-flex justify-content-center" aria-label="Options" style={{ fontSize: '1.5rem' }}></i></Button>
+            </ButtonGroup>
+          </Row>
+        </Collapse>
       <Row>
         <Col md={1} className="collapse d-md-block bg-light align-items-center text-center p-0"><NavBarFilters filters={filters}/></Col>
         <Col md={3} className="collapse d-md-block bg-light align-items-center text-center"><NavBarProjects filters={filters}/></Col>
--- biglab1/src/NavBarMobile.js
+++ biglab1/src/NavBarMobile.js
@@ -1,34 +1,15 @@
-import React, { useState } from 'react';
-import { Collapse, Navbar, Nav, ButtonGroup, Button, Form, FormControl, Image } from 'react-bootstrap';
+import { Nav, Button, Form, FormControl, Image } from 'react-bootstrap';
 
 function NavBarMobile(props) {
-    const [open, setOpen] = useState(true);
+    
 
     return (
-        <Navbar id="mobile-navbar" bg="primary" className="w-100">
-            <Button onClick={() => {}}><i className="bi bi-list-nested text-light" aria-label="Home" style={{ fontSize: '1.5rem' }}></i></Button>
-            <Collapse id="basic-navbar-nav" in={open}>
-                <Nav id="filter-navbar" className="collapse flex-column bg-primary position-fixed text-center" style={{ minHeight: '100%', width: '5rem'}}>
-                    <ButtonGroup vertical className="w-100 pt-5 align-items-center">
-                        {props.filters.map(filter => {
-                            return <Button
-                                id={`filter-${filter.label}`} 
-                                key={`filter-${filter.label}`} 
-                                className="p-3 btn-primary" 
-                                variant="link" 
-                                onClick={() => {document.getElementById('filter-title').innerText = filter.label;}}>
-                                    <i className={`bi bi-${filter.icon} text-light d-flex justify-content-center`} aria-label={filter.label} style={{ fontSize: '1.5em' }}></i>
-                            {filter.label}</Button>;
-                        })}
-                        
-                        <Button id="options" key="options" className="p-3 btn-primary w-100" variant="link"><i className="bi bi-three-dots text-light d-flex justify-content-center" aria-label="Options" style={{ fontSize: '1.5rem' }}></i></Button>
-                    </ButtonGroup>
-                </Nav>
-            </Collapse>
+        <Nav id="mobile-navbar" bg="primary" className="w-100">
+            <Button onClick={() => {props.setOpen(!props.open)}}><i className="bi bi-list-nested text-light" aria-label="Home" style={{ fontSize: '1.5rem' }}></i></Button>
             <Form inline>
                 <FormControl
                     autoFocus
-                    className="mx-2 w-auto"
+                    className="ms-auto w-auto"
                     placeholder="Type to filter..."
                     onChange={(e) => {if(e.target.value !== '')document.getElementById('filter-title').innerText = e.target.value;}}
                 />
@@ -36,7 +17,7 @@ function NavBarMobile(props) {
             <div style={{ position: "absolute", right: "2rem" }} className="p-0">
                 <Image src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" roundedCircle style={{ height: '3rem', width: 'auto' }}></Image>
             </div>
-        </Navbar>);
+        </Nav>);
 }
 
 export default NavBarMobile;
