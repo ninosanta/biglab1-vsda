621902dd5c472b6eae8c945978ca70493e50c358
Carmine De Stefano <s278176@studenti.polito.it>
2021-04-18 15:34:00 +0200

    Desktop navbar fixed
    aggiunti anche tooltip migliorabili per descrivere le icone

--- biglab1/src/Components.js
+++ biglab1/src/Components.js
@@ -7,25 +7,25 @@ function VerticalNavbar(props) {
     return (
         <>
             <Col md={1} xs={1} className="p-0">
-                <aside className="bg-primary align-items-center text-center mx-auto" id="filter-sidebar" style={{ minHeight: '100vh', minWidth: '100%' }}>
+                <aside className="bg-primary align-items-center text-center mx-auto" id="filter-sidebar" style={{position:'fixed', minHeight: '100%', width: '7rem', zIndex: '1'}}>
                     <div className="d-block d-md-none d-lg-none d-xl-none">
                         <Button onClick={() => props.setOpen(old => !old)} id="home" className="mt-4 p-0" variant="link"><i className="bi bi-list-nested text-dark" aria-label="Home" style={{ fontSize: '1.5rem' }}></i></Button>
                     </div>
                     <div>
                         <ButtonGroup vertical className="m-0 w-100 pt-5">
-                            <Button id="filter-all" className="p-3  btn-primary" variant="link"><i className="bi bi-inbox text-light d-flex justify-content-center" aria-label="All" style={{ fontSize: '1.5em' }}></i></Button>
-                            <Button id="filter-important" className="p-3 btn-primary" variant="link"><i className="bi bi-bookmark-star text-light d-flex justify-content-center" aria-label="Important" style={{ fontSize: '1.5rem' }}></i></Button>
-                            <Button id="filter-today" className="p-3 btn-primary" variant="link"><i className="bi bi-sunset text-light d-flex justify-content-center" aria-label="Today" style={{ fontSize: '1.5rem' }}></i></Button>
-                            <Button id="filter-week" className="p-3 btn-primary" variant="link"><i className="bi bi-calendar-week text-light d-flex justify-content-center" aria-label="This week" style={{ fontSize: '1.5rem' }}></i></Button>
-                            <Button id="filter-private" className="p-3 btn-primary" variant="link"><i className="bi bi-eye-slash text-light d-flex justify-content-center" aria-label="Private" style={{ fontSize: '1.5rem' }}></i></Button>
-                            <Button id="filter-search" className="p-3 btn-primary" variant="link"><i className="bi bi-search text-light d-flex justify-content-center" aria-label="Search" style={{ fontSize: '1.5rem' }}></i></Button>
+                            <Button id="filter-all" className="p-3  btn-primary" variant="link" data-toggle="tooltip" data-placement="right" title="All"><i className="bi bi-inbox text-light d-flex justify-content-center" aria-label="All" style={{ fontSize: '1.5em' }}></i></Button>
+                            <Button id="filter-important" className="p-3 btn-primary" variant="link" data-toggle="tooltip" data-placement="right" title="Important"><i className="bi bi-bookmark-star text-light d-flex justify-content-center" aria-label="Important" style={{ fontSize: '1.5rem' }}></i></Button>
+                            <Button id="filter-today" className="p-3 btn-primary" variant="link" data-toggle="tooltip" data-placement="right" title="Today"><i className="bi bi-sunset text-light d-flex justify-content-center" aria-label="Today" style={{ fontSize: '1.5rem' }}></i></Button>
+                            <Button id="filter-week" className="p-3 btn-primary" variant="link" data-toggle="tooltip" data-placement="right" title="Week"><i className="bi bi-calendar-week text-light d-flex justify-content-center" aria-label="This week" style={{ fontSize: '1.5rem' }}></i></Button>
+                            <Button id="filter-private" className="p-3 btn-primary" variant="link" data-toggle="tooltip" data-placement="right" title="Private"><i className="bi bi-eye-slash text-light d-flex justify-content-center" aria-label="Private" style={{ fontSize: '1.5rem' }}></i></Button>
+                            <Button id="filter-search" className="p-3 btn-primary" variant="link" data-toggle="tooltip" data-placement="right" title="Search"><i className="bi bi-search text-light d-flex justify-content-center" aria-label="Search" style={{ fontSize: '1.5rem' }}></i></Button>
                         </ButtonGroup>
                     </div>
                     <div>
-                        <Button id="options" className="p-3 btn-primary w-100" variant="link"><i className="bi bi-three-dots text-light d-flex justify-content-center" aria-label="Options" style={{ fontSize: '1.5rem' }}></i></Button>
+                        <Button id="options" className="p-3 btn-primary w-100" variant="link" data-toggle="tooltip" data-placement="right" title="Options"><i className="bi bi-three-dots text-light d-flex justify-content-center" aria-label="Options" style={{ fontSize: '1.5rem' }}></i></Button>
                     </div>
-                    <div style={{ position: "absolute", bottom: "2rem" }} className="p-0">
-                        <Image src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" roundedCircle style={{ width: '65%', height: 'auto' }}></Image>
+                    <div className="p-0">
+                        <Image src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" roundedCircle style={{ marginTop: '4rem', width: '4rem', height: '4rem' }}></Image>
                     </div>
                 </aside>
             </Col>
